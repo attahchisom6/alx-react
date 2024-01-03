6c969708658896cd3f38c962f6d487f3496dfa29
attahchisom6 <attahchisom@gmail.com>
2024-01-03 18:15:09 +0100

    add

---
+++ 0x09-react_redux_connectors_and_providers/task_6/dashboard/SRC/src/reducers/uiReducer.test.js
@@ -0,0 +1,80 @@
+/*
+ * @jest-environment jsdom
+ */
+
+import { Map } from "immutable";
+import {
+  unSelectCourse,
+  selectCourse,
+} from "../actions/courseActionCreators";
+import disp from "../actions/uiActionCreators";
+import reduxState from "./uiReducer";
+
+const { displayNotificationDrawer } = disp;
+const { initialState } = reduxState;
+
+describe("it tests our uiReducer", () => {
+  let uiReducerSpy;
+  let state
+
+  beforeEach(() => {
+    state = initialState;
+    uiReducerSpy = jest.spyOn(reduxState, "uiReducer");
+  });
+
+  afterEach(() => {
+    uiReducerSpy.mockRestore();
+  });
+
+  it("verifies that when no action is passed, the stata returned is the initial state", () => {
+    const newState = uiReducerSpy(state, {});
+    expect(uiReducerSpy).toHaveBeenCalledTimes(1);
+    expect(uiReducerSpy).toHaveBeenCalledWith(initialState, {});
+    expect(newState).toEqual(initialState);
+  });
+
+  it("verifies that uiReducer returns initial with the action type SELECT_COURSR", () => {
+    const action = selectCourse(3);
+    const newState = uiReducerSpy(state, action);
+
+    expect(uiReducerSpy).toHaveBeenCalled();
+    expect(uiReducerSpy).toHaveBeenCalledWith(initialState, action);
+    expect(newState).toEqual(initialState);
+  });
+
+  it("verifies that uiReducer sets isNotificationDrawerVisible: to true with the action type DISPLAY_NOTIFICATION_DRAWER", () => {
+    const action = displayNotificationDrawer();
+    const newState = uiReducerSpy(state, action);
+
+    expect(uiReducerSpy).toHaveBeenCalled();
+    expect(uiReducerSpy).toHaveBeenCalledWith(initialState, action);
+    expect(newState.toJS()).toEqual({
+      ...state.toJS(),
+      isNotificationDrawerVisible: true,
+    });
+    expect(newState.get("isNotificationDrawerVisible")).toEqual(true);
+    jest.restoreAllMocks();
+  });
+
+  it("verifies that uiReducer updates the user payload with the current email and password inputed by the user", () => {
+    const [email, password] = ["test@test.com", "password1234"];
+    const action = disp.login(email, password);
+    const newState = uiReducerSpy(state, action);
+
+    expect(uiReducerSpy).toHaveBeenCalled();
+    expect(uiReducerSpy).toHaveBeenCalledWith(initialState, action);
+    expect(newState.toJS()).toEqual({
+      ...state.toJS(),
+      user: {
+        ...state.toJS().user,
+        email,
+        password,
+      },
+      isUserLoggedIn: false,
+    });
+    expect(newState.getIn(["user", "email"])).toEqual(email);
+    expect(newState.getIn(["user", "password"])).toEqual(password);
+    expect(newState.get("isUserLoggedIn")).toEqual(false);
+    jest.restoreAllMocks();
+  });
+});
